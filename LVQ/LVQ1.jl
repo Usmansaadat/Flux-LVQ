5bede8c002c3844ffba5e00a3e888686de17945b
Usman Khan <khanusman830@gmail.com>
2020-09-26 04:12:07 +0200

    Add Euclidean Distance and Best matching function

--- LVQ/LVQ1.jl
+++ LVQ/LVQ1.jl
@@ -22,3 +22,28 @@ function test(testing_data,testing_labels)
     This function is used to test the accuracy of LVQ model using the testing data.
     """
 end
+
+function euclidean_distance(row1, row2)
+    """
+    This function calculates the Euclidean distance between the two datapoints
+    """
+    distance = 0.0
+	for i in range(len(row1)-1)
+		distance += (row1[i] - row2[i])^2
+	    return sqrt(distance)
+    end
+end
+
+
+function get_best_matching_unit(codebooks, test_row)
+    """
+    This function helps locate the best matching unit
+    """
+    distances = Any[]
+	for codebook in codebooks
+		dist = euclidean_distance(codebook, test_row)
+		append!(distances,[codebook, dist])
+    end
+	distances.sort()
+	return distances
+end
