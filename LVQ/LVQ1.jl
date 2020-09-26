ff4c8f4e61d01fefc3d59438367aa422ce916553
Usman Khan <khanusman830@gmail.com>
2020-09-26 03:38:49 +0200

    Define function bodies for LVQ, GLVQ and RSLVQ

---
+++ LVQ/LVQ1.jl
@@ -0,0 +1,24 @@
+using Flux
+using DataFrames
+using DataFramesMeta
+using Random
+using Statistics
+
+function __init__()
+    """
+    Initialization function which would initialize the LVQ model with given parameters. Other than that,
+        this function would be used to raise exceptions
+    """
+end
+
+function train(training_data,training_labels)
+    """
+    This function is used to train model using the training data.
+    """
+end
+
+function test(testing_data,testing_labels)
+    """
+    This function is used to test the accuracy of LVQ model using the testing data.
+    """
+end
